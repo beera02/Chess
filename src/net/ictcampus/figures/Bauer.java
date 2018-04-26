53387af13b44c541390f4b0847dac515f2cb1e93
Youri Weber <youri.weber@ict-campus.net>
2018-04-26 16:50:40 +0200

    Bilder von Figuren hinzugefügt.

--- src/net/ictcampus/figures/Bauer.java
+++ src/net/ictcampus/figures/Bauer.java
@@ -1,6 +1,9 @@
 package net.ictcampus.figures;
 
 public class Bauer extends Figur{
+     public Bauer(String name, String farbe) {
+          super()
+     }
 	public void bewegen() {
 		
 	}
--- src/net/ictcampus/figures/Figur.java
+++ src/net/ictcampus/figures/Figur.java
@@ -8,5 +8,9 @@ public abstract class Figur {
 	public int[][] position = new int[8][8];
 	public ImageIcon bild;
 	
+	public Figur(String name, String farbe) {
+	     this.figurFarbe = farbe;
+	     this.figurName = name;
+	}
 	public abstract void bewegen();
 }
